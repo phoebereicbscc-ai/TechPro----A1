aa7445a47eebbacf4ca82eff164f7a04a013df30
Phoebe Rei C. Bustamante <phoebereicb.scc@gmail.com>
2025-08-23 02:00:25 +0000

    Seatwork5 done

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello Java!");
+    public static void main(String[] args) {
+        System.out.println("Seatwork5");
+        int countdown = 3;
+
+        while(countdown  > 0) {
+            System.out.println(countdown);
+            countdown--;
+            
+        }
+
+        System.out.println("Hapy New Year!!");
     }
 }
