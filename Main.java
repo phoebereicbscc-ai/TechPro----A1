0473991536e84003b33694b6f511a1f1282d3469
Phoebe Rei C. Bustamante <phoebereicb.scc@gmail.com>
2025-08-30 02:36:23 +0000

    SeatworkSeven done

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
 public class Main{
-    public static void main(String[] args) {
-        System.out.println("Seatwork5");
-        int countdown = 3;
+        public static void main(String[] args) {
+                    System.out.println("Seatwork5");
+                            int countdown = 3;
 
-        while(countdown  > 0) {
-            System.out.println(countdown);
-            countdown--;
-            
+                                    while(countdown  > 0) {
+                                                    System.out.println(countdown);
+                                                                countdown--;
+                                                                            
+                                    }
+
+                                            System.out.println("Hapy New Year!!");
+                                
         }
 
-        System.out.println("Hapy New Year!!");
-    }
-}
+                                    
+        }
---
+++ Seatwork6.java
@@ -0,0 +1,27 @@
+public class Seatwork6{
+public static void main(String[] args) {
+    System. out. println("Seatwork 6");
+    int[][] lotteryCard = { {20,15,7}, 
+    {8,7,19}, 
+    {7,13,47} };
+
+    int[][] lotteryCard2 = new int [3][3];
+
+    lotteryCard2 [0] [0] = 20;
+    lotteryCard2 [0] [1] = 15;
+    lotteryCard2 [0] [2] = 7;
+    lotteryCard2 [1] [0] = 8;
+    lotteryCard2 [1] [1] = 7;
+    lotteryCard2 [1] [2] = 19;
+    lotteryCard2 [2] [0] = 7;
+    lotteryCard2 [2] [1] = 13;
+    lotteryCard2 [2] [2] = 47;
+
+    System. out. println(lotteryCard2[0][0]);
+    System. out. println("_________");
+for (int i = 0; i < 3; i++)
+{System.out.println(lotteryCard[2][2]);
+
+}
+}
+}
