11a97e883a56d0ceb462cf2668999f96f28a568e
Phoebe Rei C. Bustamante <phoebereicb.scc@gmail.com>
2025-08-30 02:36:47 +0000

    SeatworkEight done

---
+++ SeatworkSeven.java
@@ -0,0 +1,27 @@
+public class SeatworkSeven {
+
+    public static void main(String[] args) {
+        char grade = 'C';
+        
+        switch(grade){
+            case 'A':
+            System.out.println("Excellet!");
+            break;
+            case 'B':
+            case 'C':
+            System.out.println("Well done");
+            case 'D':
+            System.out.println("You passed");
+            break;
+            case 'F':
+            System.out.println("Better try again");
+            break;
+            default:
+            System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is:" + grade);
+        }
+
+    }
+    
+
