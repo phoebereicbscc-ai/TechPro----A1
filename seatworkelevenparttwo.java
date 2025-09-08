0f04ae9fefe6a5fc22f5ceba19a3fb4f78ce9442
Phoebe Rei C. Bustamante <phoebereicb.scc@gmail.com>
2025-09-08 02:49:04 +0000

    seatworkeleven done

---
+++ seatworkelevenparttwo.java
@@ -0,0 +1,9 @@
+public class seatworkelevenparttwo {
+    	public static void main(String[] args) {
+        		System.out.println("SEATWORKELEVENPARTTWO2025");
+                		int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
+                        		myNumbers[1][2] = 9;
+                                		System.out.println(myNumbers[1][2]);
+                                        	}
+                                            }
+}
